a758d4ce7f87ab5ec51c92f74a912dd0090a7a5d
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2019-03-04 11:04:55 +0100

    user

---
+++ src/app/users/shared/user.model.ts
@@ -0,0 +1,7 @@
+export interface User {
+    id?: string;
+    username: string;
+    regDate: Date;
+    profilePicUrl?: string;
+    profilePicId?: string;
+}
--- src/app/users/shared/user.service.ts
+++ src/app/users/shared/user.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { FileService } from 'src/app/files/shared/file.service';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { FileMeta } from 'src/app/files/shared/file-meta.model';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,21 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class UserService {
 
   constructor(private fs: FileService, private db: AngularFirestore) { }
+
+  createUser(user: User): Observable<User> {
+    return from(this.db.collection('users').add(
+      {
+        username: user.username,
+        regDate: Date.now()
+      }
+    )
+    ).pipe(
+      map(userRef => {
+        user.id = userRef.id;
+        return user;
+      })
+    )
+  }
 
   uploadProfileImage(blob: Blob, type: string, name: string): Observable<FileMeta> {
     /*if (blob) {
