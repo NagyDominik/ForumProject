43c81620b3efc8cd1e08bcf96b8751853eb67447
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2019-04-29 14:10:47 +0200

    Profile page design, New post improvements

--- src/app/forumposts/forumpost-create/forumpost-create.component.ts
+++ src/app/forumposts/forumpost-create/forumpost-create.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { Forumpost } from '../shared/forumpost.model';
 import { ForumpostsService } from '../shared/forumposts.service';
-import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-forumpost-create',
   templateUrl: './forumpost-create.component.html',
   styleUrls: ['./forumpost-create.component.scss']
 })
-export class ForumpostCreateComponent implements OnInit {
+export class ForumpostCreateComponent implements OnInit, AfterViewInit {
 
   PostForm = new FormGroup({
     title: new FormControl(''),
@@ -21,28 +23,47 @@ export class ForumpostCreateComponent implements OnInit {
 
   constructor(private fps: ForumpostsService,
     private router: Router,
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute,
+    private snackBar: MatSnackBar,
+    private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
+
   clear() {
     this.PostForm.reset();
+    this.fileToUpload = null;
   }
 
   setUploadFile(event) {
-    this.fileToUpload = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(this.fileToUpload);
-    reader.onload = () => {
-      this.imgURL = reader.result;
-    };
+    if (event.target.files[0].type.includes('image')) {
+      this.fileToUpload = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(this.fileToUpload);
+      reader.onload = () => {
+        this.imgURL = reader.result;
+      };
+    } else {
+      this.snackBar.open('Only image files are accepted', 'OK', {
+        duration: 3000
+      });
+    }
   }
 
   post() {
     const post: Forumpost = this.PostForm.value;
-    this.fps.createPost(post, this.fileToUpload);
-    this.back();
+    if ((post.title != null && post.title !== '') && (this.fileToUpload == null || this.fileToUpload.type.includes('image'))) {
+      this.fps.createPost(post, this.fileToUpload);
+      this.back();
+    } else {
+      this.snackBar.open('Cannot create post without title', 'OK', {
+        duration: 3000
+      });
+    }
   }
 
   back() {
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatSnackBarModule } from '@angular/material';
+import { MatSnackBarModule, MatButtonModule } from '@angular/material';
 import { ImageCropperModule } from 'ngx-image-cropper';
 
 import { UserProfileComponent } from './user-profile/user-profile.component';
@@ -14,7 +15,9 @@ import { UsersRoutingModule } from './users-routing.module';
     UsersRoutingModule,
     ImageCropperModule,
     ReactiveFormsModule,
-    MatSnackBarModule
+    FlexLayoutModule,
+    MatSnackBarModule,
+    MatButtonModule
   ]
 })
 export class UsersModule { }
