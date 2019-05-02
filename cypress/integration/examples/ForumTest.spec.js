3187a5080acfd598e44defd5e04b0b3901c720eb
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2019-05-02 15:42:13 +0200

    cypress done, pop style is nicelooking

--- cypress/integration/examples/ForumTest.spec.js
+++ cypress/integration/examples/ForumTest.spec.js
@@ -1,10 +1,7 @@
 
 describe('ForumPost list page tests', () => {
-  it('Navigate the main page', () => {
+  it('Navigate to the main page', () => {
    cy.visit('http://localhost:4200/');
-  });
-  it('mat card subtitle should contains p which is the postDate', ()=> {
-    cy.get('mat-card-subtitle').should('contain','p');
   });
   it('mat card content should contains image which is the posted image', ()=> {
       cy.get("img");
@@ -22,7 +19,7 @@ describe('ForumPost list page tests', () => {
 });
 
 describe('ForumPost page test with post new text based post and delete it', () => {
-  it('Navigate the create page ', () => {
+  it('Navigate to the create page ', () => {
     cy.visit('http://localhost:4200/create');
     cy.get('#mat-tab-label-0-1').click();
     cy.get('#mat-tab-label-0-0').click();
@@ -32,15 +29,52 @@ describe('ForumPost page test with post new text based post and delete it', () =
     cy.get('input[formControlName="title"]')
     .invoke('val')
     .then(sometext => cy.log(sometext));
+    cy.wait(1000);
   });
   it('get the input text from the input description field of a new text based post', ()=> {
     cy.get('textarea[formControlName="description"]').type('this is a cypress test');
     cy.get('textarea[formControlName="description"]')
       .invoke('text')
       .then(sometext => cy.log(sometext));
+    cy.wait(1000);
   });
   it('Should find the post button and click it', ()=>{
     cy.get('#postBtn').click({force:true});
+    cy.wait(1000);
+  });
+});
+
+describe('ForumPost title update through Pop up dialog', () => {
+  it('Navigate to the main page ', () => {
+    cy.visit('http://localhost:4200/');
+  });
+
+  it('it should find the edit button and edit the first one', ()=>{
+      cy.get('mat-card').first();
+      cy.get('#moreMenuBtn').click();
+      cy.get('#editPost').click({force:true});
+    cy.wait(1000);
+  });
+  it('it should click on cancel button to cancel the pop up dialog', ()=> {
+    cy.get('#cancelBtn').click({force:true});
+    cy.wait(1000);
+  });
+  it('it should find the edit button and edit the first one', ()=>{
+    cy.get('mat-card').first();
+    cy.get('#moreMenuBtn').click();
+    cy.get('#editPost').click({force:true});
+    cy.wait(1000);
+  });
+  it('get the input text from the input title field for update post title', ()=> {
+    cy.get('input[formControlName="newTitle"]').type(' with cypress');
+    cy.get('input[formControlName="newTitle"]')
+      .invoke('val')
+      .then(sometext => cy.log(sometext));
+    cy.wait(1000);
+  });
+  it('it should click on save button to save the new title', ()=> {
+    cy.get('#saveBtn').click({force:true});
+    cy.wait(1000);
   });
   it('it should find the delete button and delete the first one', ()=>{
     cy.get('mat-card').first();
--- src/app/forumposts/forumposts-list/forumposts-list.component.ts
+++ src/app/forumposts/forumposts-list/forumposts-list.component.ts
@@ -40,7 +40,6 @@ export class ForumpostsListComponent implements OnInit {
       data: updatePost,
       disableClose: true,
       autoFocus: true,
-      width: '350px',
       }
     );
 
