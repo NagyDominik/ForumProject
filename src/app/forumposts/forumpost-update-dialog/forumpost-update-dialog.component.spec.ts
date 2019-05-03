4035144aafda21c79ff07ec315e01fad1863a0c6
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2019-05-03 13:12:24 +0200

    forumpost update dialog tests finished

--- src/app/forumposts/forumpost-update-dialog/forumpost-update-dialog.component.spec.ts
+++ src/app/forumposts/forumpost-update-dialog/forumpost-update-dialog.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule, MatFormFieldModule, MatDialogRef, MAT_DIALOG_DATA, MatInputModule } from '@angular/material';
 
 import { ForumpostUpdateDialogComponent } from './forumpost-update-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ForumpostUpdateDialogComponent', () => {
   let component: ForumpostUpdateDialogComponent;
   let fixture: ComponentFixture<ForumpostUpdateDialogComponent>;
+  let dialogRefMock: any;
 
   beforeEach(async(() => {
+    dialogRefMock = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     TestBed.configureTestingModule({
-      declarations: [ ForumpostUpdateDialogComponent ]
+      declarations: [ ForumpostUpdateDialogComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatDialogModule,
+        MatInputModule
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRefMock },
+        { provide: MAT_DIALOG_DATA, useValue: {title: 'Test'}},
+      ]
     })
     .compileComponents();
   }));
@@ -19,8 +36,23 @@ describe('ForumpostUpdateDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('save', () => {
+    it('should call dialogRef close with new title as argument', () => {
+      component.PostForm.get('newTitle').setValue('Test Title');
+      component.save();
+      expect(dialogRefMock.close).toHaveBeenCalledWith('Test Title');
+    });
+  });
+
+  describe('cancel', () => {
+    it('should call dialogRef close without arguments', () => {
+      component.cancel();
+      expect(dialogRefMock.close).toHaveBeenCalledWith();
+    });
+  });
 
 });
